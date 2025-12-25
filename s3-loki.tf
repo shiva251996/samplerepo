d3ea0767edcc5a2816060d84a39dfcc0f0aa3b0c
shivam <abdevisiva251996@gmail.com>
2025-12-25 12:07:48 +0530

    Update s3-loki.tf

--- s3-loki.tf
+++ s3-loki.tf
@@ -6,10 +6,26 @@ resource "aws_s3_bucket_lifecycle_configuration" "loki_lifecycle" {
   bucket = aws_s3_bucket.loki.id
 
   rule {
-    id     = "30-days"
+    id     = "loki-expire"
     status = "Enabled"
 
+    filter {
+      # set the prefix for objects this rule applies to, e.g. "loki/" or "" for entire bucket
+      prefix = ""
+      # OR use an 'and' block for prefix + tags:
+      # and {
+      #   prefix = "loki/"
+      #   tags = {
+      #     "some-tag" = "value"
+      #   }
+      # }
+    }
+
     expiration {
+      days = 365
+    }
+
+    noncurrent_version_expiration {
       days = 30
     }
   }
